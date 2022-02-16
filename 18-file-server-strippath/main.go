8f844a8f27049f9c8fa248ca8d62f472e4a56d08
pichayuth kittisriswai <pichayuthkitti@gmail.com>
2022-02-16 18:40:16 +0700

    serve file from a specific folder

---
+++ 18-file-server-strippath/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.Handle("/resources", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
+	http.ListenAndServe(":8080", nil)
+}
