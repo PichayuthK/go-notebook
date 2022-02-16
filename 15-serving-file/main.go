d083083fc3633f125e3640c6fd56f4c6099d170c
pichayuth kittisriswai <pichayuthkitti@gmail.com>
2022-02-16 10:09:10 +0700

    copy file over

---
+++ 15-serving-file/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/dog.jpeg", getImage)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `
+	<img src="/dog.jpeg">
+	`)
+}
+
+func getImage(w http.ResponseWriter, r *http.Request) {
+	f, err := os.Open("dog.jpeg")
+	if err != nil {
+		http.Error(w, "file not found", 404)
+		return
+	}
+	defer f.Close()
+
+	io.Copy(w, f)
+}
